626f351de6ccfb31487fe8b1905904728045a5ea
binbin <binbin.zou@dxc.com>
2019-11-08 13:42:01 +0800

    Echo some info,pass the jenkins setupWizard by init plugin

--- Jenkin/jenkins_master_run.sh
+++ Jenkin/jenkins_master_run.sh
@@ -1,14 +1,50 @@
 # if network behind proxy, need add -e "JAVA_OPTS=-Dhttp.proxyHost=xxxx -Dhttp.proxyPort=xxxx -Dhttps.proxyHost=xxxx -Dhttps.proxyPort=xxxx"
+echo "-------------------------------------------"
+echo "--------Jenkins Master Run Begining--------"
+echo "-------------------------------------------"
+
+echo "init some information for run jenkins master"
 bash jenkins_dockerconfig.sh
+
+echo "-------------------------------------------"
+echo "--------Jenkins Master Running-------------"
+echo "-------------------------------------------"
+echo "docker run jenkins master"
 docker run  -p 10000:8080 --name jenkins -v /opt/maven/settings.xml:/usr/share/maven/conf/settings.xml  -v /var/run/docker.sock:/var/run/docker.sock  -v /etc/localtime:/etc/localtime:ro -v /opt/jenkins_home:/var/jenkins_home -d  jenkins:master &
 
+
+echo "-------------------------------------------"
+echo "--------Jenkins Plugins Runing-------------"
+echo "----------Just For Copy jpi----------------"
+echo "-------------------------------------------"
+echo "sleeping 4s"
 # sleep
-sleep 10s
+sleep 4s
+echo "docker run jenkins plugin"
 docker run -d --name plugin binbin0126/jenkinsplugin:latest
-sleep 5s
+echo "sleeping 2s"
+sleep 2s
+echo "cp jpi files from docker to hostpath"
 docker cp plugin:/home/plugin.tar.gz /opt/jenkins_home	
+echo "taring.............."
 tar -zxvf /opt/jenkins_home/plugin.tar.gz -C /opt/jenkins_home/plugins
-sleep 5s
+echo "sleeping 2s"
+sleep 2s
+echo "-------------------------------------------"
+echo "--------Jenkins Master Restarting----------"
+echo "-------------------------------------------"
+echo "docker restart jenkins master"
+docker restart jenkins
+echo "sleep 2s"
+sleep 2s
+echo "init some information for plugin init"
 bash jenkins_dockerconfig.sh
+echo "-------------------------------------------"
+echo "---------Jenkins Plugins Remove------------"
+echo "-------------------------------------------"
+echo "docker stop plugin"
 docker stop plugin
+echo "docker rm plugin"
 docker rm plugin
+echo "jenkins master is Running"
+echo "Done"
