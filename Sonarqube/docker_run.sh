0ecdf0692c386ba048907c9f31a8ffa9cf9d8944
root <root@ip-172-31-39-91.ap-northeast-1.compute.internal>
2019-11-24 04:44:46 +0000

    change images

--- Sonarqube/docker_run.sh
+++ Sonarqube/docker_run.sh
@@ -1,2 +1,2 @@
 # change the sonarque by self
-docker run -d --name sonarqube  -p 9000:9000 -p 9092:9092 registry-vpc.cn-shanghai.aliyuncs.com/devopsimages/robot:latest
+docker run -d --name sonarqube  -p 9000:9000 -p 9092:9092 registry.cn-shanghai.aliyuncs.com/devopsimages/robot:latest
